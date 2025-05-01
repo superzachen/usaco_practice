e3d59db5e8ee87675c751917e0d68087f0e99194
Zach Chen <zachary.zh.chen@gmail.com>
2025-04-30 20:40:15 -0700

    Format 855

--- bronze/855_mixing_milk.py
+++ bronze/855_mixing_milk.py
@@ -5,16 +5,16 @@ cap3, buk3 = map(int, input().split())
 TURN_NUM = 100
 
 for i in range(1, TURN_NUM + 1):
-    if i%3 == 1:
-        amount_changed = min(buk1, cap2-buk2)
+    if i % 3 == 1:
+        amount_changed = min(buk1, cap2 - buk2)
         buk1 -= amount_changed
         buk2 += amount_changed
-    if i%3 == 2:
-        amount_changed = min(buk2, cap3-buk3)
+    if i % 3 == 2:
+        amount_changed = min(buk2, cap3 - buk3)
         buk2 -= amount_changed
         buk3 += amount_changed
-    if i%3 == 0:
-        amount_changed = min(buk3, cap1-buk1)
+    if i % 3 == 0:
+        amount_changed = min(buk3, cap1 - buk1)
         buk3 -= amount_changed
         buk1 += amount_changed
 
